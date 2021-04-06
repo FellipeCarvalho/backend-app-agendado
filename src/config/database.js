f1f9b94241e47c7804e438a1db4277088220575a
Fellipe Carvalho <fellipealbert3@gmail.com>
2021-04-06 18:04:05 -0300

    changed config db

--- src/config/database.js
+++ src/config/database.js
@@ -13,7 +13,17 @@ const url = process.env.URL_DB
 //acessa uma função para definir a url e colocar que é compativel com outras versões
 //mongoose.connect(url, { useNewUrlParser: true });
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}); //alterei para essa nova instrução a de cima estava depreciada
+//mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}); //alterei para essa nova instrução a de cima estava depreciada
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  }).then(res=>{
+          console.log("DB Connected!")
+  }).catch(err => {
+    console.log(Error, err.message);
+  })
 
 //exporta o uma variável para poder usar esse arquivo em outros locais
 module.exports = mongoose;
